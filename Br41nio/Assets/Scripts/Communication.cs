a1b19dc3e5cd564f920d824502adf325b1ca0bbe
c-tsalidis <ctsalidis1999@gmail.com>
2021-10-17 17:35:48 +0200

    Started with the calculation of the baseline of frequencies for personal calibration

--- Br41nio/Assets/Scripts/Communication.cs
+++ Br41nio/Assets/Scripts/Communication.cs
@@ -1,14 +1,26 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using UnityEditor.UIElements;
 
 public class Communication : MonoBehaviour {
+    
+    [Header("UDP networking")]
     private const int ListenPort = 1000;
     private IPEndPoint _groupEp;
     private Socket _socket;
+    
+    [Space]
+    
+    [Header("Frequency bands")]
     public float [] frequencyBandsAverages = new float[7];
+    public float [] frequencyBandsBaselines = new float[7];
+
+    public bool baselineWaitingTimeFinished = false;
+    private int _baseLineCounter = 0;
 
     /*
     Unicorn EEG powerband .NET API averaged values
@@ -23,7 +35,10 @@ public class Communication : MonoBehaviour {
     63: gamma channel 1-8 averaged
      */
 
-    private void Start() => StartListener();
+    private void Start() {
+        StartListener();
+        StartCoroutine(GetValuesForBaseline());
+    }
 
     private void Update() {
         // Check out the Unicorn UDP receiver --> https://github.com/unicorn-bi/Unicorn-Suite-Hybrid-Black/blob/master/Unicorn%20.NET%20API/UnicornUDP/UnicornUDPReceiver/Program.cs
@@ -34,14 +49,17 @@ public class Communication : MonoBehaviour {
             Array.Copy(receiveBufferByte, messageByte, numberOfBytesReceived);
             string message = Encoding.ASCII.GetString(messageByte);
             var split = message.Split(',');
-            var counter = 0;
+            var frequencyBandsCounter = 0;
             for (int i = 56; i < 63; i++) {
-                frequencyBandsAverages[counter] = float.Parse(split[i]);
-                counter++;
+                if(baselineWaitingTimeFinished) {
+                    frequencyBandsAverages[frequencyBandsCounter] = float.Parse(split[i]);
+                    frequencyBandsCounter++;
+                }
+                else {
+                    frequencyBandsBaselines[_baseLineCounter] += float.Parse(split[i]);
+                    _baseLineCounter++;
+                }
             }
-            // _thetaAverage = float.Parse(split[58 - 1]); // get the theta averaged
-            // _alphaAverage = float.Parse(split[59 - 1]); // get the alpha averaged
-            // Debug.Log(split.Length + " | " + _thetaAverage + " | " + _alphaAverage);
         }
     }
 
@@ -49,6 +67,18 @@ public class Communication : MonoBehaviour {
         _groupEp = new IPEndPoint(IPAddress.Any, ListenPort);
         _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp) {Blocking = false};
         _socket.Bind(_groupEp);
+    }
+
+    private IEnumerator GetValuesForBaseline() {
+        yield return new WaitForSeconds(5.0f);
+        CalculateBaseline();
+        baselineWaitingTimeFinished = true;
+    }
+
+    private void CalculateBaseline() {
+        for (int i = 0; i < frequencyBandsBaselines.Length; i++) {
+            frequencyBandsBaselines[i] = frequencyBandsBaselines[i] / _baseLineCounter;
+        }
     }
 
     private void OnDisable() => _socket.Close();
