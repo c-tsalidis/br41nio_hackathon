a03ec784f0331d603d19421ec6ae87b0c457134c
c-tsalidis <ctsalidis1999@gmail.com>
2021-10-17 14:04:03 +0200

    Update Communication.cs

--- Br41nio/Assets/Scripts/Communication.cs
+++ Br41nio/Assets/Scripts/Communication.cs
@@ -1,4 +1,3 @@
-/*
 using System;
 using UnityEngine;
 using System.Net;
@@ -6,25 +5,33 @@ using System.Net.Sockets;
 using System.Text;
 
 public class Communication : MonoBehaviour {
-    private const int ListenPort = 11000;
+    private const int ListenPort = 1000;
     private UdpClient _listener;
     private IPEndPoint _groupEp;
+
+    private Socket socket;
+    private byte[] receiveBufferByte = new byte[1024];
+    private float[] receiveBufferFloat;
 
     private void Start() => StartListener();
 
     private void Update() {
+        int numberOfBytesReceived = socket.Receive(receiveBufferByte);
+        if (numberOfBytesReceived > 0) {
+            //convert byte array to float array
+            for (int i = 0; i < numberOfBytesReceived / sizeof(float); i++) {
+                receiveBufferFloat[i] = BitConverter.ToSingle(receiveBufferByte, i * sizeof(float));
+                Debug.Log(receiveBufferFloat[i].ToString("n2"));
+            }
+        }
+        /*
         byte[] bytes = _listener.Receive(ref _groupEp);
-        byte[] receiveBufferByte = new byte[1024];
-        float[] receiveBufferFloat = new float[receiveBufferByte.Length / sizeof(float)];
         try {
             if (bytes.Length > 0) {
                 //convert byte array to float array
                 for (int i = 0; i < bytes.Length / sizeof(float); i++) {
                     receiveBufferFloat[i] = BitConverter.ToSingle(receiveBufferByte, i * sizeof(float));
-                    //if (i + 1 < bytes.Length / sizeof(float))
-                    //    Debug.Log(receiveBufferFloat[i].ToString("n2"));
-                    //else
-                        Debug.Log(receiveBufferFloat[i].ToString("n2"));
+                    Debug.Log(receiveBufferFloat[i].ToString("n2"));
                 }
             }
 
@@ -32,9 +39,10 @@ public class Communication : MonoBehaviour {
             Debug.Log(Encoding.ASCII.GetString(bytes, 0, bytes.Length));
         }
         catch (SocketException e) {
-            Debug.LogError(e);
+            // Debug.LogError(e);
             Application.Quit();
         }
+        */
 
         // Check out the Unicorn UDP receiver --> https://github.com/unicorn-bi/Unicorn-Suite-Hybrid-Black/blob/master/Unicorn%20.NET%20API/UnicornUDP/UnicornUDPReceiver/Program.cs
     }
@@ -43,76 +51,13 @@ public class Communication : MonoBehaviour {
         _listener = new UdpClient(ListenPort);
         _groupEp = new IPEndPoint(IPAddress.Any, ListenPort);
         _listener.Client.Blocking = false;
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp) {Blocking = false};
+        socket.Bind(_groupEp);
+        receiveBufferFloat = new float[receiveBufferByte.Length / sizeof(float)];
     }
 
     private void OnDisable() => _listener.Close();
     private void OnDestroy() => _listener.Close();
 
     private void OnApplicationQuit() => _listener.Close();
-}
-*/
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-
-
-public class Communication : MonoBehaviour {
-    private const int listenPort = 11000;
-    private UdpClient listener;
-    private IPEndPoint groupEP;
-    
-    private void Start() {
-        StartListener();
-    }
-
-    private void Update() {
-        try {
-            // Debug.Log("Waiting for broadcast");
-            byte[] bytes = listener.Receive(ref groupEP);
-            var receivedMessage = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
-            print(receivedMessage);
-            byte[] receiveBufferByte = new byte[1024];
-            float[] receiveBufferFloat = new float[receiveBufferByte.Length / sizeof(float)];
-            try {
-                if (bytes.Length > 0) {
-                    //convert byte array to float array
-                    for (int i = 0; i < bytes.Length / sizeof(float); i++) {
-                        receiveBufferFloat[i] = BitConverter.ToSingle(receiveBufferByte, i * sizeof(float));
-                        //if (i + 1 < bytes.Length / sizeof(float))
-                        //    Debug.Log(receiveBufferFloat[i].ToString("n2"));
-                        //else
-                        Debug.Log(receiveBufferFloat[i].ToString("n2"));
-                    }
-                }
-
-                Debug.Log("Received broadcast from " + groupEP);
-                Debug.Log(Encoding.ASCII.GetString(bytes, 0, bytes.Length));
-            }
-            catch (SocketException e) {
-                Debug.LogError(e);
-                Application.Quit();
-            }
-            Debug.Log($"Received broadcast from {groupEP} :");
-            Debug.Log($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
-        }
-        catch (SocketException e) {
-            // Debug.LogError(e);
-        }
-    }
-
-    private void StartListener() {
-        listener = new UdpClient(listenPort);
-        groupEP = new IPEndPoint(IPAddress.Any, listenPort);
-        listener.Client.Blocking = false;
-    }
-
-    private void OnDisable() => listener.Close();
-    private void OnDestroy() => listener.Close();
-
-    private void OnApplicationQuit() => listener.Close();
 }
